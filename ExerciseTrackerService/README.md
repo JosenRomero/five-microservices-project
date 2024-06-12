00ff01ecf3267174884d6116d98cc79ac2900180
Jose <romerojosemanuel@hotmail.com>
2024-06-12 17:32:49 -0600

    add ExerciseTrackerService

---
+++ ExerciseTrackerService/README.md
@@ -0,0 +1,3 @@
+# Exercise Tracker
+
+It was built based on the boilerplate available [here](https://github.com/freeCodeCamp/boilerplate-project-exercisetracker/)
---
+++ ExerciseTrackerService/index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express()
+
+app.set("port", process.env.PORT || 3000)
+
+app.use(express.urlencoded({ extended: true })); 
+app.use(cors())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`)
+});
+
+app.post('/api/users', (req, res) => {
+
+  res.json({
+    username: req.body.username,
+    _id: "123456"
+  })
+
+});
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+
+  const _id = req.params._id
+
+  const { description, duration, date } = req.body
+
+  res.json({
+    username: "test",
+    description,
+    duration,
+    date,
+    _id
+  })
+
+});
+
+// Starting the server
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`)
+});
