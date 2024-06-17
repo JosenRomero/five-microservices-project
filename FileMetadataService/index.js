54b22115fd0d1fe5725b100add7e295077bdabb5
Jose <romerojosemanuel@hotmail.com>
2024-06-16 18:22:27 -0600

    feat(FileMetadataService): upload a file functionality

--- FileMetadataService/index.js
+++ FileMetadataService/index.js
@@ -1,6 +1,8 @@
+import fs from 'node:fs' // file system
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import multer from './config/multerConfig.js'
 
 const app = express()
 
@@ -11,6 +13,27 @@ app.use('/public', express.static(`${process.cwd()}/public`))
 
 app.get('/', (req, res) => {
   res.sendFile(`${process.cwd()}/views/index.html`)
+});
+
+app.post('/api/fileanalyse', multer.single('upfile'), async (req, res) => {
+
+  try {
+
+    const { originalname, mimetype, size, path: filePath } = req.file
+
+    // delete the file of the uploads folder 
+    await fs.promises.unlink(filePath)
+
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size: size
+    })
+
+  } catch(err) {
+    res.status(500).send("Something went wrong.")
+  }
+
 });
 
 // Starting the server
